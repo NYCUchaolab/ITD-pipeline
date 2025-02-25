24f205db20bef5f6e3b9d8eda595835970ce00fa
Juan-Jeffery <jjj853714@gmail.com>
2025-02-25 10:29:16 +0800

    fix some vep shell bug

--- Run_merge_caller.sh
+++ Run_merge_caller.sh
@@ -1,31 +1,36 @@
 #!/bin/sh
 
-#
-start_number=1
-end_number=678
-cancer="GBM"
+cancer=$1
+start_number=$2
+end_number=$3
 
-#
+
+
 source $ITD_PIPELINE_CONFIG
-
+source $TOOLS
 #
 eval "$(conda shell.bash hook)"
-conda activate $vep113_ENV
+conda activate $pyITD_ENV
 
-#
-base_dir="/staging/biology/u4583512/TCGA_whole/${cancer}/modified_vcf"
-mkdir -p "$base_dir/genomonITD" "$base_dir/pindel" "$base_dir/scanITD" "$base_dir/merge_caller"
+CANCER_DIR=${BASE_DIR}/${cancer}
+echo $CANCER_DIR
+
+# check_and_create_dir $CANCER_DIR modified_vcf
+
 
 # 
 for number in $(seq "$start_number" "$end_number"); do
     echo "Processing sample ${number} for cancer type ${cancer}..."
     
-    # run merge_caller.py
-    python scripts/merge_caller.py \
-        -s "/staging/biology/u4583512/TCGA_whole/slice_sample_sheet/${cancer}/${cancer}_sample_${number}.tsv" \
-        -i "/staging/biology/u4583512/TCGA_whole/${cancer}/raw_data" \
-        -o "$base_dir"
+    echo "$pyITD_SAMPLE_SHEET_DIR/${cancer}_sample_${number}.tsv"
+    echo "$CANCER_DIR/raw_data"
+    echo "$CANCER_DIR/modified_vcf"
 
+    # run merge_caller.py
+    python $PIPELINE_DIR/scripts/merge_caller.py \
+        -s "$pyITD_SAMPLE_SHEET_DIR/${cancer}_sample_${number}.tsv" \
+        -i "$CANCER_DIR/raw_data" \
+        -o "$CANCER_DIR/modified_vcf"
     # wait 180 sec
     #sleep 180
 done
--- Run_vep.sh
+++ Run_vep.sh
@@ -1,42 +1,45 @@
 #!/bin/sh
 
 #
-start_number=1
-end_number=5
-cancer="GBM"
+cancer=$1
+start_number=$2
+end_number=$3
 
 #
 source $ITD_PIPELINE_CONFIG
-
+source $TOOLS
+source $READ_SAMPLE_SHEET
 #
 eval "$(conda shell.bash hook)"
-conda activate $pyITD_ENV
+conda activate $vep113_ENV
+
+VERBOSE=1
+INPUT_DIR=$BASE_DIR/${cancer}/modified_vcf/merge_caller
+OUTPUT_DIR=$BASE_DIR/${cancer}/vep
+
+mkdir -p "$OUTPUT_DIR"  "$OUTPUT_DIR/split_vcf" "$OUTPUT_DIR/vep_vcf" "$OUTPUT_DIR/vep_maf"
 
 #
-input_dir="/staging/biology/u4583512/TCGA_whole/${cancer}/modified_vcf/merge_caller"
-output_dir="/staging/biology/u4583512/TCGA_whole/${cancer}/vep_result"
-
-mkdir -p "$output_dir"  "$output_dir/split_vcf" "$output_dir/vep_vcf" "$output_dir/vep_maf"
-
-#
-vcf_files=($(ls "$input_dir"/*.vcf))
-if [ ${#vcf_files[@]} -eq 0 ]; then
-    echo "Error: No VCF files found in $input_dir"
-    exit 1
-fi
+# vcf_files=($(ls $INPUT_DIR/*.vcf))
+# if [ ${#vcf_files[@]} -eq 0 ]; then
+#     echo "Error: No VCF files found in $INPUT_DIR"
+#     exit 1
+# fi
 
 #
 for number in $(seq "$start_number" "$end_number"); do
-    case_id=$(basename "${vcf_files[$((number-1))]}" .vcf)
-
-    echo "Processing sample ${number} (${case_id}) for cancer type ${cancer}..."
+    parse_sample_sheet $pyITD_SAMPLE_SHEET_DIR/${cancer}_sample_${number}.tsv
+    
+    # case_id=$(basename "${vcf_files[$((number-1))]}" .vcf)
+    check_file_existence "${Case_ID} vcf" $INPUT_DIR/${Case_ID}.merged.vcf
+    echo "Processing sample ${number} (${Case_ID}) for cancer type ${cancer}..."
 
     # run vcf2maf.pl
-    perl scripts/vcf2maf.pl \
-        --input-vcf "${input_dir}/${case_id}.vcf" \
-        --output-maf "${output_dir}/vep_maf/${case_id}.maf" \
-        --normal-id "${case_id}_N" \
-        --tumor-id "${case_id}_T" \
+    perl $PIPELINE_DIR/scripts/vcf2maf.pl \
+        --input-vcf "${INPUT_DIR}/${Case_ID}.merged.vcf" \
+        --output-maf "${OUTPUT_DIR}/vep_maf/${Case_ID}.merged.maf" \
+        --normal-id "${Case_ID}_N" \
+        --tumor-id "SAMPLE" \
         --ref-fasta $vep113_REF \
         --vep-data $vep113_DATA \
         --vep-path $vep113_PATH \
@@ -45,12 +48,12 @@ for number in $(seq "$start_number" "$end_number"); do
         --vep-overwrite
         
     #move file
-    if [ -f "${input_dir}/${case_id}.vep.vcf" ]; then
-        mv "${input_dir}/${case_id}.vep.vcf" "$output_dir/vep_vcf/"
+    if [ -f "${INPUT_DIR}/${Case_ID}.merged.vep.vcf" ]; then
+        mv "${INPUT_DIR}/${Case_ID}.merged.vep.vcf" "$OUTPUT_DIR/vep_vcf/"
     fi
     
-    if [ -f "${input_dir}/${case_id}.split.vcf" ]; then
-        mv "${input_dir}/${case_id}.split.vcf" "$output_dir/split_vcf/"
+    if [ -f "${INPUT_DIR}/${Case_ID}.merged.split.vcf" ]; then
+        mv "${INPUT_DIR}/${Case_ID}.merged.split.vcf" "$OUTPUT_DIR/split_vcf/"
     fi
     
 done
